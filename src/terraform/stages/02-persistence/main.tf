dab0fcd5d5548ce85aa634f50c629799bcbe526f
Alessandro Vozza <alessandro.vozza@microsoft.com>
2021-12-22 12:11:41 +0100

    id

--- src/terraform/stages/02-persistence/main.tf
+++ src/terraform/stages/02-persistence/main.tf
@@ -25,7 +25,7 @@ module "acr" {
   resource_group_name      = data.azurerm_resource_group.rg.name
   location                 = data.azurerm_resource_group.rg.location
   resource_naming_template = local.resource_naming_template
-  subnet_id                = data.azurerm_subnet.hubprivate
+  subnet_id                = data.azurerm_subnet.hubprivate.id
   private_vnet_id          = data.azurerm_virtual_network.hub.id
   tags                     = var.tags
 }
