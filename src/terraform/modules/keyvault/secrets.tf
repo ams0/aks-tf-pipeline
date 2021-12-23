d0cb6eb17e196a151ddd1d7c394817aa596e2548
Alessandro Vozza <alessandro.vozza@microsoft.com>
2021-12-23 23:34:16 +0100

    comment

--- src/terraform/modules/keyvault/secrets.tf
+++ src/terraform/modules/keyvault/secrets.tf
@@ -1,4 +1,4 @@
-resource "random_password" "keycloak_password" {
+#resource "random_password" "keycloak_password" {
 #     length           = 16
 #     special          = true
 #     override_special = "_%@"
@@ -8,21 +8,21 @@ resource "random_password" "keycloak_password" {
 #     lower            = true
 #     min_lower        = 2
 #   }
-  
+
 #   resource "random_string" "keycloak_login" {
 #     length  = 16
 #     special = false
 #   }
-  
+
 #   resource "time_static" "now" {}
-  
+
 #   resource "azurerm_key_vault_secret" "keycloak_username" {
 #     name            = "keycloak-username"
 #     value           = random_string.keycloak_login.result
 #     key_vault_id    = azurerm_key_vault.keyvault.id
 #     tags            = { secret = false }
 #     expiration_date = timeadd(time_static.now.rfc3339, "2160h")
-  
+
 #     lifecycle {
 #       ignore_changes = [
 #         not_before_date,
@@ -30,14 +30,14 @@ resource "random_password" "keycloak_password" {
 #       ]
 #     }
 #   }
-  
+
 #   resource "azurerm_key_vault_secret" "keycloak_password" {
 #     name            = "keycloak-password"
 #     value           = random_password.keycloak_password.result
 #     key_vault_id    = azurerm_key_vault.keyvault.id
 #     tags            = { secret = true }
 #     expiration_date = timeadd(time_static.now.rfc3339, "2160h")
-  
+
 #     lifecycle {
 #       ignore_changes = [
 #         not_before_date,
@@ -45,4 +45,3 @@ resource "random_password" "keycloak_password" {
 #       ]
 #     }
 #   }
-  
