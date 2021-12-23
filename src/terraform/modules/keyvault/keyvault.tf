cfa7ce016bdb1bc4b3180bc372db7b347f68044b
Alessandro Vozza <alessandro.vozza@microsoft.com>
2021-12-23 15:14:59 +0100

    expiration for secret

--- src/terraform/modules/keyvault/keyvault.tf
+++ src/terraform/modules/keyvault/keyvault.tf
@@ -87,16 +87,34 @@ resource "random_string" "keycloak_login" {
   special = false
 }
 
+resource "time_static" "now" {}
+
 resource "azurerm_key_vault_secret" "keycloak_username" {
-  name         = "keycloak-username"
-  value        = random_string.keycloak_login.result
-  key_vault_id = azurerm_key_vault.keyvault.id
-  tags         = { secret = false }
+  name            = "keycloak-username"
+  value           = random_string.keycloak_login.result
+  key_vault_id    = azurerm_key_vault.keyvault.id
+  tags            = { secret = false }
+  expiration_date = timeadd(time_static.now.rfc3339, "2160h")
+
+  lifecycle {
+    ignore_changes = [
+      not_before_date,
+      expiration_date
+    ]
+  }
 }
 
 resource "azurerm_key_vault_secret" "keycloak_password" {
-  name         = "keycloak-password"
-  value        = random_password.keycloak_password.result
-  key_vault_id = azurerm_key_vault.keyvault.id
-  tags         = { secret = true }
+  name            = "keycloak-password"
+  value           = random_password.keycloak_password.result
+  key_vault_id    = azurerm_key_vault.keyvault.id
+  tags            = { secret = true }
+  expiration_date = timeadd(time_static.now.rfc3339, "2160h")
+
+  lifecycle {
+    ignore_changes = [
+      not_before_date,
+      expiration_date
+    ]
+  }
 }
