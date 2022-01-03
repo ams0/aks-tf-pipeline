d8b7829b891ad0c91c6bd920a15278d31de704bc
Alessandro Vozza <alessandro.vozza@microsoft.com>
2022-01-03 12:23:09 +0100

    ws-based appin

--- src/terraform/modules/insights/insights.tf
+++ src/terraform/modules/insights/insights.tf
@@ -1,7 +1,38 @@
+
+resource "azurerm_log_analytics_workspace" "logs" {
+  name                = format(var.resource_naming_template, var.progressive, "log")
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
+
+  tags = var.tags
+
+}
+
+resource "azurerm_log_analytics_solution" "container" {
+  count                 = length(var.solutions)
+  solution_name         = "ContainerInsights"
+  location              = var.location
+  resource_group_name   = var.resource_group_name
+  workspace_resource_id = azurerm_log_analytics_workspace.logs.id
+  workspace_name        = azurerm_log_analytics_workspace.logs.name
+
+  plan {
+    publisher = "Microsoft"
+    product   = "OMSGallery/ContainerInsights"
+  }
+
+  tags = var.tags
+}
+
+
 resource "azurerm_application_insights" "insights" {
   name                = format(var.resource_naming_template, var.progressive, "appi")
   location            = var.location
   resource_group_name = var.resource_group_name
   application_type    = "Node.JS"
-  tags                = var.tags
+  workspace_id        = azurerm_log_analytics_workspace.logs.id
+
+  tags = var.tags
 }
