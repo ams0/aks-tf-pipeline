22eef84f63cc903feb7406a50bd10629d3f9211c
Alessandro Vozza <alessandro.vozza@microsoft.com>
2022-01-03 14:17:04 +0100

    nodepool in same subnet

--- src/terraform/modules/aksgitops/nodepools.tf
+++ src/terraform/modules/aksgitops/nodepools.tf
@@ -19,7 +19,8 @@ resource "azurerm_kubernetes_cluster_node_pool" "pools" {
   eviction_policy      = var.nodepools[count.index].eviction_policy
   availability_zones   = var.nodepools[count.index].availability_zones
 
-  vnet_subnet_id      = var.nodepools[count.index].vnet_subnet_id
+  vnet_subnet_id       = var.vnet_subnet_id
+  #vnet_subnet_id      = var.nodepools[count.index].vnet_subnet_id
   enable_auto_scaling = var.nodepools[count.index].enable_auto_scaling
 
   node_labels = var.nodepools[count.index].node_labels
